fe8bf947417c3bb9b912878ae0b9ad18aa6dfd84
n5qvfliszw <cutlet80hayseed@icloud.com>
2025-12-08 09:15:00 +0800

    cleanup: remove deprecated config and hooks

--- src/config/wagmi.ts
+++
@@ -1,9 +0,0 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { sepolia } from 'wagmi/chains';
-
-export const config = getDefaultConfig({
-  appName: 'LiquidSwap - FHE-Powered DEX',
-  projectId: 'YOUR_PROJECT_ID', // Get from https://cloud.walletconnect.com
-  chains: [sepolia],
-  ssr: false,
-});
